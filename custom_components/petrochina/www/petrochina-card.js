e0f31544d3707e729b9a98d41add7d18f8f91063
xiaoshi <115949669+xiaoshi930@users.noreply.github.com>
2025-12-27 17:56:05 +0800

    Update petrochina-card.js

--- custom_components/petrochina/www/petrochina-card.js
+++ custom_components/petrochina/www/petrochina-card.js
@@ -1,4 +1,4 @@
-console.info("%c 消逝-油价卡 \n%c      v 2.4 ", "color: red; font-weight: bold; background: black", "color: white; font-weight: bold; background: dimgray");
+console.info("%c 消逝-油价卡 \n%c      v 2.5 ", "color: red; font-weight: bold; background: black", "color: white; font-weight: bold; background: dimgray");
 
 import { LitElement, html, css } from "https://unpkg.com/lit-element@2.4.0/lit-element.js?module";
 
